ffc067629f0945c15f162f45779d164a4de21f81
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-07-01 05:36:42 +0100

    more set up ongoing.

---
+++ app/redux/Provider.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from './store'
+import { Toaster } from 'react-hot-toast'
+
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const storeRef = useRef<AppStore>()
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore()
+  }
+
+  return (
+    <>
+      <Toaster
+        position='top-center'
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            zIndex: 20000,
+            fontSize: 14
+          }
+        }}
+      />
+      <Provider store={storeRef.current}>{children}</Provider>
+    </>
+  )
+}
