41f61c9580454f99838e0e03e6db968ac1da6979
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-07-10 21:08:30 +0100

    feat: app layout done.

--- app/(pages)/dashboard/layout.tsx
+++ app/(pages)/dashboard/layout.tsx
@@ -2,15 +2,17 @@ import Aside from '@/app/components/Dashboard/Aside';
 import Header from '@/app/components/Dashboard/Header';
 import React from 'react'
 
-const AppLayout = ({children}: Readonly<{
-    children: React.ReactNode;
-  }>) => {
+const AppLayout = ({ children }: Readonly<{
+  children: React.ReactNode;
+}>) => {
   return (
-    <div className='bg-appGrey/50'>
-        <Header />
-        <Aside />
-        <main className=""></main>
+    <div className='bg-appGrey/50 h-screen'>
+      <Header />
+      <Aside />
+      <main className="ml-[250px] p-8">
         {children}
+      </main>
+
     </div>
   )
 }
--- app/components/Dashboard/Aside.tsx
+++ app/components/Dashboard/Aside.tsx
@@ -3,8 +3,8 @@ import Logo from '../Logo/Logo'
 
 const Aside = () => {
   return (
-    <aside className='max-w-xs w-[245px] bg-white h-screen overflow-x-hidden overflow-y-auto'>
-        <div className="">
+    <aside className='max-w-xs w-[245px] bg-white h-screen overflow-x-hidden overflow-y-auto fixed left-0 top-0 '>
+        <div className="h-[4rem] grid place-items-center">
             <Logo />
         </div>
     </aside>
