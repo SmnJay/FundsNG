ad6bcfb7b5c38eaba72822e4cd0c6b6ca080015e
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-07-11 05:19:17 +0100

    update to mobile responsiveness

--- app/components/Dashboard/Aside.tsx
+++ app/components/Dashboard/Aside.tsx
@@ -5,12 +5,12 @@ import Navigation from '../Navigation'
 
 const Aside = () => {
   return (
-    <aside className='max-w-xs w-[245px] border-r-2  bg-white h-screen overflow-x-hidden overflow-y-auto fixed left-0 top-0 '>
+    <aside className='max-w-xs w-[245px] border-r-2 bg-white h-screen overflow-x-hidden overflow-y-auto fixed left-0 top-0 max-md:hidden'>
       <div className="h-[4rem] grid place-items-center">
         <Logo />
       </div>
 
-      <div className="flex justify-center items-center gap-2">
+      <div className="flex justify-center items-center gap-2 mt-6">
         <Avatar />
         <div className="">
           <h3 className="font-semibold">Tosin Akerele</h3>
--- app/components/Navigation/index.tsx
+++ app/components/Navigation/index.tsx
@@ -9,7 +9,7 @@ const Navigation: React.FC = () => {
   const activePath = usePathname();
 
     return (
-        <nav className='space-y-4 w-2/3 mx-auto py-8'>
+        <nav className='space-y-4 w-4/5 mx-auto py-8'>
             {
                 data.map((item) => {
                     const { id, name, link, icon } = item;
@@ -26,7 +26,7 @@ const Navigation: React.FC = () => {
                                 </div>
                             }
                             ariaLabel={name} 
-                            cls={`${activePath === link && 'bg-appGrey'} text-primary block capitalize font-medium leading-loose hover:bg-appGrey rounded-lg ease-linear duration-200 py-1 px-2 outline outline-transparent hover:outline-[#FFECE5]s`}
+                            cls={`${activePath === link && 'bg-appGrey'} text-primary block capitalize font-medium leading-loose hover:bg-appGrey rounded-lg ease-linear duration-200 py-2 px-6 outline outline-transparent hover:outline-[#FFECE5]s`}
                         />
                     )
                 })
