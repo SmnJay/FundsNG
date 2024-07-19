177c98b0e8fb83eb443b672c11afa03de7fea21f
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-07-19 04:54:51 +0100

    changes.

--- app/components/Cards/index.tsx
+++ app/components/Cards/index.tsx
@@ -12,7 +12,7 @@ type ICards = {
 
 const Cards: React.FC<ICards> = ({ icon, bgColor, amountColor, amount, title, titleColor }) => {
     return (
-        <div className={`rounded-lg md:rounded-xl p-4 md:p-6 flex items-center justify-between gap-2 ${bgColor ? bgColor : 'bg-primary'}`}>
+        <div className={`rounded-md p-4 md:p-6 flex items-center justify-between gap-2 ${bgColor ? bgColor : 'bg-primary'}`}>
             <div className="font-bvp space-y-4">
                 <h5 className={`text-sm font-medium ${titleColor ? titleColor : 'text-secondary'}`}>{title}</h5>
                 {amount && <p className={`font-medium text-xl md:text-2xl ${amountColor ? amountColor : 'text-white'}`}>&#8358;{Number(amount).toLocaleString()}</p>}
--- app/components/Dashboard/DashboardWrapper.tsx
+++ app/components/Dashboard/DashboardWrapper.tsx
@@ -11,21 +11,21 @@ import { MdBusinessCenter } from 'react-icons/md';
 
 const DashboardWrapper = () => {
     const [isEmpty, setIsEmpty] = useState(true);
-    const handleIsEmpty = () => setIsEmpty(!isEmpty)
+    const handleIsEmpty = () => setIsEmpty(!isEmpty);
+
     return (
         <div>
             <button onClick={handleIsEmpty} className="p-2 bg-leafGreen-30 mb-4 text-white font-bvp text-sm">{isEmpty ? 'Check Not-Empty State' : 'Check empty State'}</button>
 
             {
-                isEmpty ? <div className="flex max-sm:flex-col items-end sm:items-center justify-between rounded-md px-4 py-2 bg-[#EBF7DF]">
+                isEmpty ? <div className="flex max-sm:flex-col items-end sm:items-center justify-between rounded-md px-4 py-2 bg-[#EBF7DF] w-2/3">
                     <p className="font-medium text-leafGreen-20">Before you continue, complete your profile</p>
                     <Link href='/settings/profile' className='text-sm text-leafGreen-30 font-medium bg-white rounded-lg py-2 leading-loose px-4'>
                         Complete Profile
                     </Link>
                 </div>
                     :
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 mt-4 md:mt-8">
-
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  mt-4 md:mt-8">
                         <Cards
                             title='Total Donations made'
                             amount={'1500387'}
@@ -58,8 +58,9 @@ const DashboardWrapper = () => {
                         />
                     </div>
             }
+
             {
-                isEmpty ? <div className="mt-4 flex items-center justify-center sm:justify-between rounded-md bg-signUp-pattern bg-leafGreen-20 text-white p-4 sm:p-8">
+                isEmpty ? <div className="w-2/3 mt-4 flex items-center justify-center sm:justify-between rounded-md bg-signUp-pattern bg-leafGreen-20 text-white p-4 sm:p-8">
                     <div className='space-y-3'>
                         <h1 className="font-semibold text-xl">Welcome to FundsNg</h1>
                         <p className="font-thin">
@@ -77,100 +78,127 @@ const DashboardWrapper = () => {
                             <path d="M71.3807 69.7039C71.2705 69.6865 71.1671 69.6822 71.1117 69.6551C70.5098 69.597 69.9051 69.5485 69.3007 69.511L67.7885 69.4346C67.7646 69.4191 67.5703 69.4539 67.5507 69.4357C66.7336 69.4164 65.9155 69.4209 65.0997 69.4552L64.2344 69.5048C63.4806 69.5581 62.7278 69.6369 61.9798 69.7483C58.5878 70.2547 55.3632 71.3489 52.2938 72.9438C51.7641 73.22 51.2976 72.3838 51.8299 72.1076C55.2425 70.3335 58.8484 69.134 62.6381 68.6919C65.5258 68.3549 68.4354 68.4179 71.3231 68.704C71.3769 68.7064 71.4308 68.7112 71.4862 68.7195C71.9122 68.7635 72.3365 68.8117 72.7616 68.8639C75.5964 69.2141 78.4059 69.7765 81.2509 70.0417C84.1158 70.3096 87.1389 70.3788 89.8935 69.3717C92.0388 68.5877 95.1894 67.5064 94.4953 64.6116C93.8256 61.8124 90.893 61.2304 88.4971 60.5143C85.3002 59.5594 81.9519 59.5061 78.6531 59.5994C75.3769 59.6939 72.0929 59.0619 68.8157 58.9735C65.729 58.8899 62.5898 57.6047 59.8789 55.9554C59.8459 55.9423 59.8114 55.9241 59.7781 55.8997C59.7633 55.889 59.7484 55.8777 59.7334 55.867C59.7288 55.8646 59.7242 55.8622 59.7209 55.8587C59.7092 55.8528 59.7014 55.844 59.6909 55.8367C55.2268 52.5931 50.4132 48.3908 44.7389 48.204C41.7501 48.106 38.8624 48.959 35.96 49.583C34.5362 49.8895 33.0654 50.1987 31.6083 50.23C30.1594 50.2614 28.7115 50.2359 27.2625 50.2276C23.884 50.207 20.5301 50.3986 17.3423 51.6926C14.3019 52.9274 11.3924 54.5269 8.72416 56.502C6.05012 58.4819 3.76912 60.8666 1.41368 63.2355C1.26327 63.3869 1.11261 63.5383 0.960938 63.6889L13.309 83.1397C14.0445 82.5725 14.8098 82.0503 15.6096 81.5887C18.5019 79.9201 21.6192 79.141 24.915 79.1906C28.26 79.2401 31.6142 79.8689 34.8672 80.6518C38.5065 81.5289 42.102 82.5869 45.7102 83.59C49.2986 84.5861 52.9117 85.5496 56.5725 86.199C60.1175 86.8265 63.9094 87.1975 67.4531 86.3563C67.4877 86.348 67.5212 86.3442 67.5545 86.3431C70.9143 84.2226 73.7042 81.305 76.4691 78.4136C77.848 76.9717 79.2258 75.5246 80.6841 74.1725C81.8055 73.1329 82.9659 72.0433 84.2474 71.1964C80.7126 71.1905 77.1437 70.4356 73.6424 69.9496L71.3807 69.7039Z" fill="white" />
                             <path d="M77.0972 56.9951C77.1229 56.994 77.1486 56.9932 77.1743 56.9924V56.9222C77.1488 56.9469 77.1224 56.9705 77.0972 56.9951Z" fill="white" />
                         </svg>
-
                     </div>
                 </div> : null
             }
 
 
-            <div className="bg-white rounded-md mt-4">
-                <div className="border-b px-4 sm:px-8 py-2 sm:py-4 flex items-center justify-between">
-                    <p className="font-medium text-[#899192]">Your Fundraiser</p>
-                    <Link href='/dashboard/campaigns' className='text-sm '>View All</Link>
-                </div>
-                <div className="flex items-center justify-center p-4 sm:p-8">
-                    {/* <div className="grid place-items-center space-y-6">
-            <p className="text-center max-w-[400px] mx-auto text-gray-600">You currently do not have any ongoing campaign. <br />Create one to see them here</p>
-            <ButtonLink
-              href='/dashboard/campaigns'
-              name='Start a Campaign'
-              ariaLabel='Start a campaign button'
-              color='leafGreen'
-            />
-          </div> */}
-                    <div className="">
-                        <Image src={'/images/underbridge.png'} width={400} height={450} alt='' />
-                    </div>
-                    <div className="font-bvp">
-                        <h6 className="">Save the homeless people at Ikeja underbridge</h6>
-                        <p className="">Please help the people living here find a new home or realive some of their suffering</p>
-                        <p className="font-medium">
-                            <span className="font-bvp">&#8358;132,000</span>
-                            <span className="text-gray-600">has been donated so far</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+                <div className="col-span-2">
+                    <div className="bg-white rounded-md">
+                        <div className="border-b px-4 sm:px-8 py-2 sm:py-4 flex items-center justify-between">
+                            <p className="font-medium text-[#899192]">Your Fundraiser</p>
+                            <Link href='/dashboard/campaigns' className='text-sm hover:underline'>View All</Link>
+                        </div>
+                        <div className="flex items-center justify-center p-4 sm:p-8">
+                            {
+                                isEmpty ?
+                                    <div className="grid place-items-center space-y-6">
+                                        <p className="text-center max-w-[400px] mx-auto text-gray-600">You currently do not have any ongoing campaign. <br />Create one to see them here</p>
+                                        <ButtonLink
+                                            href='/dashboard/campaigns'
+                                            name='Start a Campaign'
+                                            ariaLabel='Start a campaign button'
+                                            color='leafGreen'
+                                        />
+                                    </div>
+                                    :
+                                    <div className='flex items-center gap-6'>
+                                        <div className="">
+                                            <Image src={'/images/underbridge.png'} width={300} height={350} alt='' />
+                                        </div>
+                                        <div className="font-bvp">
+                                            <h6 className="font-semibold leading-loose text-base text-[#3f4343] mb-1">Save the homeless people at Ikeja underbridge</h6>
+                                            <p className="text-[#899192] text-sm">Please help the people living here find a new home or realive some of their suffering</p>
+                                            <div className="mb-2 mt-4 h-4 w-full bg-appGrey rounded"></div>
+                                            <p className="font-medium space-x-1 leading-loose">
+                                                <span className="font-semibold text-[#3f4343]">&#8358;132,000</span>
+                                                <span className="text-[#899192]">has been donated so far</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                            }
 
-            <div className="bg-white rounded-md mt-4">
-                <div className="border-b px-4 sm:px-8 py-2 sm:py-4 flex items-center justify-between">
-                    <p className="font-medium text-[#899192]">Trending Fundraiser Categories</p>
-                    <Link href='' className='text-sm '>View All</Link>
-                </div>
-                <div className="flex items-center justify-evenly gap-4 flex-wrap p-4 sm:p-8">
-                    <div className="sm:space-y-2 text-center">
-                        <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
-                            <FaBriefcaseMedical className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
-                        </span>
-                        <span className="text-gray-600 block">Medical</span>
-                    </div>
-                    <div className="sm:space-y-2 text-center">
-                        <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
-                            <GrEmergency className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
-                        </span>
-                        <span className="text-gray-600 block">Emergency</span>
-                    </div>
-                    <div className="sm:space-y-2 text-center">
-                        <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
-                            <FaHome className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
-                        </span>
-                        <span className="text-gray-600 block">Housing</span>
-                    </div>
-                    <div className="sm:space-y-2 text-center">
-                        <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
-                            <FaHandHoldingHeart className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
-                        </span>
-                        <span className="text-gray-600 block">Charity</span>
-                    </div>
-                    <div className="sm:space-y-2 text-center">
-                        <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
-                            <MdBusinessCenter className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
-                        </span>
-                        <span className="text-gray-600 block">Business</span>
-                    </div>
-                    <div className="sm:space-y-2 text-center">
-                        <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
-                            <FaPlaneDeparture className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
-                        </span>
-                        <span className="text-gray-600 block">Travel</span>
-                    </div>
-                </div>
-            </div>
 
-            <div className="bg-white rounded-md mt-4">
-                <div className="border-b px-4 sm:px-8 py-2 sm:py-4 flex items-center justify-between">
-                    <p className="font-medium text-[#899192]">Your Savings</p>
-                    <Link href='/dashboard/savings' className='text-sm '>View All</Link>
+                        </div>
+                    </div>
+
+                    <div className="bg-white rounded-md mt-4">
+                        <div className="border-b px-4 sm:px-8 py-2 sm:py-4 flex items-center justify-between">
+                            <p className="font-medium text-[#899192]">Trending Fundraiser Categories</p>
+                            <Link href='' className='text-sm hover:underline'>View All</Link>
+                        </div>
+                        <div className="flex items-center justify-evenly gap-4 flex-wrap p-4 sm:p-8">
+                            <div className="sm:space-y-2 text-center">
+                                <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
+                                    <FaBriefcaseMedical className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
+                                </span>
+                                <span className="text-gray-600 block">Medical</span>
+                            </div>
+                            <div className="sm:space-y-2 text-center">
+                                <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
+                                    <GrEmergency className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
+                                </span>
+                                <span className="text-gray-600 block">Emergency</span>
+                            </div>
+                            <div className="sm:space-y-2 text-center">
+                                <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
+                                    <FaHome className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
+                                </span>
+                                <span className="text-gray-600 block">Housing</span>
+                            </div>
+                            <div className="sm:space-y-2 text-center">
+                                <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
+                                    <FaHandHoldingHeart className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
+                                </span>
+                                <span className="text-gray-600 block">Charity</span>
+                            </div>
+                            <div className="sm:space-y-2 text-center">
+                                <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
+                                    <MdBusinessCenter className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
+                                </span>
+                                <span className="text-gray-600 block">Business</span>
+                            </div>
+                            <div className="sm:space-y-2 text-center">
+                                <span className="bg-leafGreen-80 mx-auto grid place-items-center h-14 sm:h-20 w-14 sm:w-20 rounded-full">
+                                    <FaPlaneDeparture className='text-[#618F19] h-6 w-6 sm:h-10 sm:w-10' />
+                                </span>
+                                <span className="text-gray-600 block">Travel</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-white rounded-md mt-4">
+                        <div className="border-b px-4 sm:px-8 py-2 sm:py-4 flex items-center justify-between">
+                            <p className="font-medium text-[#899192]">Your Savings</p>
+                            <Link href='/dashboard/savings' className='text-sm hover:underline'>View All</Link>
+                        </div>
+                        <div className="flex items-center justify-center p-4 sm:p-8">
+                            <div className="grid place-items-center space-y-6">
+                                <p className="text-center max-w-[400px] mx-auto text-gray-600">You currently do not have any ongoing savings. <br />Create one to see them here</p>
+                                <ButtonLink
+                                    href='/dashboard/savings'
+                                    name='Start Saving'
+                                    ariaLabel='Start Saving'
+                                    color='primary'
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex items-center justify-center p-4 sm:p-8">
-                    <div className="grid place-items-center space-y-6">
-                        <p className="text-center max-w-[400px] mx-auto text-gray-600">You currently do not have any ongoing savings. <br />Create one to see them here</p>
-                        <ButtonLink
-                            href='/dashboard/savings'
-                            name='Start Saving'
-                            ariaLabel='Start Saving'
-                            color='primary'
-                        />
+                <div className={`col-span-1 bg-white h-full rounded-md p-4 ${isEmpty && 'hidden'}`}>
+                    <h6 className='font-medium mb-4 leading-loose'>Recent Activity</h6>
+                    <div className="flex items-start gap-2">
+                        <div className="">
+                            <span className="rounded-full flex items-center justify-center font-bold h-10 w-10 bg-zinc-300 p-2">OM</span>
+                        </div>
+                        <div className="space-y-2">
+                            <p className="text-[#899192] font-medium">
+                                <span className='text-[#20525C]'>Akintola</span> donated <span className='text-[#20525C]'>12,500</span> to the campaign
+                            </p>
+                            <p className="text-[#4591A1] font-medium">Reply with special message</p>
+                            <p className="text-[#899192]">10:34 | 2 April, 2024</p>
+                        </div>
                     </div>
                 </div>
             </div>
