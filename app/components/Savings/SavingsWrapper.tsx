e4107dbdaa4e6e8530c4af9ae186273524332de4
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-07-27 21:14:29 +0100

    feat:

--- app/components/Savings/SavingsWrapper.tsx
+++ app/components/Savings/SavingsWrapper.tsx
@@ -69,13 +69,12 @@ const SavingsWrapper = () => {
                                 </div>
                                 <div className="font-bvps pr-2">
                                     <h5 className={`text-sm md:text-base font-medium text-primary`}>Create new target savings</h5>
-                                    <span className="py-2s block group-hover:translate-x-2 ease-linear duration-150">
+                                    <span className="pt-2 block group-hover:translate-x-2 ease-linear duration-150">
                                         <svg width="50" height="16" viewBox="0 0 50 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M49.7071 8.70711C50.0976 8.31658 50.0976 7.68342 49.7071 7.29289L43.3431 0.928932C42.9526 0.538408 42.3195 0.538408 41.9289 0.928932C41.5384 1.31946 41.5384 1.95262 41.9289 2.34315L47.5858 8L41.9289 13.6569C41.5384 14.0474 41.5384 14.6805 41.9289 15.0711C42.3195 15.4616 42.9526 15.4616 43.3431 15.0711L49.7071 8.70711ZM0 9H49V7H0L0 9Z" fill="#4591A1" />
                                         </svg>
                                     </span>
                                 </div>
-
                             </div>
                         </div>
 
