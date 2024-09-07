4e38bd746f2fc53f110a2c95f6794297745a71b6
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-09-07 13:00:20 +0100

    build fix

--- app/(pages)/campaign/page.tsx
+++ app/(pages)/campaign/page.tsx
@@ -2,7 +2,7 @@ import Button, { ButtonLink } from '@/app/components/Button/Button'
 import PreviewCampaign from '@/app/components/Campaigns/PreviewCampaign'
 import ProgressBar from '@/app/components/ProgressBar'
 import Image from 'next/image'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { FaHourglassHalf, FaUserCircle } from 'react-icons/fa'
 import { IoIosGift, IoMdShareAlt } from 'react-icons/io'
 import { MdOutlineKeyboardDoubleArrowRight } from 'react-icons/md'
@@ -10,8 +10,10 @@ import { RiCalendarTodoFill } from 'react-icons/ri'
 
 const page = () => {
     return (
-        <PreviewCampaign />
-        
+        <Suspense>
+            <PreviewCampaign />
+        </Suspense>
+
     )
 }
 
