6ae163bf70583f61a2ea13e2e4d48bbcedea6b94
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-09-08 07:45:06 +0100

    clean up

--- app/(pages)/campaign/page.tsx
+++ app/(pages)/campaign/page.tsx
@@ -1,19 +1,11 @@
-import Button, { ButtonLink } from '@/app/components/Button/Button'
 import PreviewCampaign from '@/app/components/Campaigns/PreviewCampaign'
-import ProgressBar from '@/app/components/ProgressBar'
-import Image from 'next/image'
 import React, { Suspense } from 'react'
-import { FaHourglassHalf, FaUserCircle } from 'react-icons/fa'
-import { IoIosGift, IoMdShareAlt } from 'react-icons/io'
-import { MdOutlineKeyboardDoubleArrowRight } from 'react-icons/md'
-import { RiCalendarTodoFill } from 'react-icons/ri'
 
 const page = () => {
     return (
         <Suspense>
             <PreviewCampaign />
         </Suspense>
-
     )
 }
 
