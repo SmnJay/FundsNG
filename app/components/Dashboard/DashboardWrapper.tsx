834869b7a178d8317eda870fe0f620138d89479c
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-09-14 14:07:22 +0100

    feat: update

--- app/components/Dashboard/DashboardWrapper.tsx
+++ app/components/Dashboard/DashboardWrapper.tsx
@@ -32,6 +32,35 @@ const DashboardWrapper: React.FC<DashboardWrapperProps> = ({ isProfileSetUp }) =
 
     return (
         <div>
+            <div className="md:mt-8 lg:w-11/12 lg:mx-auto bg-white flex items-center gap-4 relative overflow-hidden">
+                <Image src='/images/dashboard-banner.png' alt='dashboard banner' width={365} height={332} />
+                <div className="">
+                    <h2 className="font-bold text-2xl">Helping People in need around the World</h2>
+                    <div className="mt-3">
+                        <div className="grid grid-cols-2 mb-3">
+                            <div className="">
+                                <div className="flex items-center gap-2">
+                                    <span className="inline-block bg-[#E0FAFF] rounded-full h-6 w-6"></span>
+                                    <span className="text-[#5C6269] font-medium">Donate</span>
+                                </div>
+                                <p className="text-xs py-2">Providing assistance in the form of money
+                                    and clothing to help others</p>
+                            </div>
+                            <div className="">
+                                <div className="flex items-center gap-2">
+                                    <span className="inline-block bg-[#F2F5A5] rounded-full h-6 w-6"></span>
+                                    <span className="text-[#5C6269] font-medium">Assist</span>
+                                </div>
+                                <p className="text-xs py-2">Providing assistance in the form of money
+                                    and clothing to help others</p>
+                            </div>
+                        </div>
+                        <ButtonLink ariaLabel='create a campaign link' cls='w-1/3' href='/dashboard/campaigns/create' name='Create a Campaign' color='leafGreen' />
+                    </div>
+                </div>
+                <div className="w-[150px]"></div>
+                <Image src='/images/right-dashboard-ellipse.png' alt='dashboard banner' width={365} height={332} className='absolute right-0 translate-x-10' />
+            </div>
             {
                 !isProfileSetUp ? <div className="flex max-sm:flex-col sm:items-center gap-2 sm:justify-between items-start rounded-md px-4 md:px-8 py-2 bg-[#FFEDD2] lg:w-11/12 lg:mx-auto  border border-[#D16A0C] text-[#D16A0C]">
                     <p className="max-md:text-sm font-light">Before you continue, please complete your profile</p>
