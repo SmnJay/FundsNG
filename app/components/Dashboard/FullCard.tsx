34342cc18f7f3c365110b15aee3865b7fab729aa
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-09-14 10:31:30 +0100

    feat: full big card done

--- app/components/Dashboard/FullCard.tsx
+++ app/components/Dashboard/FullCard.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
+import { GiCash } from "react-icons/gi";
+import { GoArrowUpRight } from 'react-icons/go';
+import { PiPiggyBankLight } from 'react-icons/pi'
+
 
 const FullCard = () => {
     return (
@@ -16,20 +20,47 @@ const FullCard = () => {
                 <button className="rounded-full px-8 hover:brightness-105 duration-150 ease-in-out py-2 text-sm bg-[#494949] text-white">Crowdfunding</button>
                 <button className="rounded-full px-8 hover:brightness-105 duration-150 ease-in-out py-2 text-sm bg-[#d9d9d9] text-[#404040]">Target Savings</button>
             </div>
-            <div className="grid grid-cols-6 gap-6">
-                <div className="">adsfadsf</div>
-                <div className="relative">
-                    <Image className="" src='/images/left.png' alt='support education' width={128} height={150} />
-                    <p className="absolute bottom-0 text-white p-4 text font-medium">Support <br />Linda&apos;s <br />Education</p>
+            <div className="grid grid-cols-6 gap-3">
+                <div className="grid grid-rows-6 gap-3">
+                    <div className="row-span-4 relative">
+                        <Image src={'/images/left-bg.png'} alt='background' className='w-full' width={160} height={193} />
+                        <div className="absolute top-0 left-0 bottom-0 right-0 p-4 flex justify-center flex-col">
+                            <span className="text-[#A7E2E8] font-bold text-2xl">78%</span>
+                            <p className="font-light text-[#CAE3E4] text-xs leading-6 py-2">Campaigns reach their goals faster with FundsNg</p>
+                            <button className="text-[11px] flex items-center justify-between rounded-full px-2 py-1 text-white bg-[#00C2E23D] w-full">
+                                Start Campaign <GoArrowUpRight className='text-black bg-[#55C1CB] rounded-full p-1' size={25} />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="bg-[#2B2B2B] text-white rounded-xl p-4 row-span-2 flex items-center justify-center gap-2"><GiCash size={20} className='flex-shrink-0' /> FundRaiser</div>
                 </div>
-                <div className="rounded-lg text-center col-start-3 col-span-2 bg-[#E9E9E9] text-black p-4 mb-0 mt-auto h-2/3 flex flex-col items-center justify-center">
-                    <h4 className="font-semibold">Join 8,000+ <br />People Donate</h4>
+                <div style={{ alignContent: 'end' }} className="relative">
+                    <span className="absolute bottom-0 text-white text font-medium p-4 text-lg">Support <br />Linda&apos;s <br />Education</span>
+                    <Image className="w-full" src='/images/left.png' alt='support education' width={128} height={150} />
                 </div>
-                <div className="relative">
-                    <Image className="" src={'/images/right.png'} alt='Feed people project' width={128} height={150} />
-                    <p className="absolute font-medium bottom-0 text-white p-4">Project: Feed 5000 people Bi-monthly</p>
+                <div className="rounded-xl text-center col-start-3 col-span-2 bg-[#E9E9E9] text-black p-4 mb-0 mt-auto h-3/5 flex flex-col items-center justify-center space-y-3">
+                    <h4 className="font-semibold text-2xl">Join 8,000+ <br />People Donate</h4>
+                    <Image src='/images/avatars.png' alt='group avatars' className='scale-125' width={120} height={30} />
                 </div>
-                <div className="">adsfadsf</div>
+                <div style={{ alignContent: 'end' }} className="relative ">
+                    <span className="absolute font-medium bottom-0 text-white p-4">Project: Feed 5000 people Bi-monthly</span>
+                    <Image className="w-full" src={'/images/right.png'} alt='Feed people project' width={128} height={150} />
+                </div>
+                <div className="grid grid-rows-6 gap-3">
+                    <div className="row-span-4 relative">
+                        <Image src={'/images/right-bg.png'} alt='background' className='w-full' width={160} height={193} />
+                        <div className="absolute top-0 left-0 bottom-0 right-0 p-4 flex justify-center text-center flex-col">
+                            <Image src='/images/right-bg-image.png' width={105} height={85} alt='image' className='text-center mx-auto mb-2'/>
+                            <p className="text-white text-center font-extralight text-sm">Add Families and friends to savings</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#153207] text-[#D7F1B1] font-light rounded-xl p-4 row-span-2 flex items-center justify-center gap-2">
+                        <PiPiggyBankLight size={32} className='flex-shrink-0' />
+                        <span className="text-sm">
+                            Savings at your own pace
+                        </span>
+                    </div>
+                </div>
             </div>
         </section>
     )
