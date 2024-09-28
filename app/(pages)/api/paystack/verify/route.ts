1e3daf7a268fcb6a6def9a46d3a262ae776893cf
Babajide Sunmonu <sunmonu.babajide@fundsng.com>
2024-09-28 19:55:47 +0100

    feat: verify done too.

--- app/(pages)/api/paystack/verify/route.ts
+++ app/(pages)/api/paystack/verify/route.ts
@@ -10,7 +10,6 @@ export async function GET(req: NextRequest, res: NextResponse) {
     const version = searchParams.get('version') || '1'; // Default to '1' if not provided
     const amount = searchParams.get('amount') || ''; // Extracted from request query
 
-
     const headers = {
         Authorization: `Bearer ${jwt}`,
         ApiKey: process.env.APIKEY,
@@ -20,9 +19,9 @@ export async function GET(req: NextRequest, res: NextResponse) {
     };
 
     try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/Paystack/${reference}/Verify`, { headers });
-
-        return NextResponse.json(response.data.data, { status: 200 })
+        const url = `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/Paystack/${reference}/Verify`;
+        const response = await axios.get(url, { headers });
+        return NextResponse.json(response.data, { status: 200 })
     } catch (error: any) {
         const err = error.response.data.message;
 
